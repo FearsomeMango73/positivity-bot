fd84aca412d71f9f44085298c5b818ad4fc5c0a3
Hayley Christianson <hayleychristianson@gmail.com>
2018-04-15 01:44:10 -0500

    changed the main function for it to run online
    It is now currently running on Fruitful Discussions and will need to figure our future commands.
    
    In order to run the bot: right click main (in the string[] args line) and click run.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,21 @@
+//package main;
+
 import net.dv8tion.jda.core.JDABuilder;
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
 import javax.security.auth.login.LoginException;
+
 public class Main extends ListenerAdapter {
 
-    public static void Main(String[] args) throws LoginException{
+    public static void main(String[] args) throws LoginException{
         JDABuilder builder = new JDABuilder(AccountType.BOT);
-        String token = "NDM0ODI4NzY2MzQ3NTI2MTQ1";
+        String token = "NDM0ODI4NzY2MzQ3NTI2MTQ1.DbQWzw.mY_YkwWN_chyztyCUPo_uWrdbek";
         builder.setToken(token);
         builder.addEventListener(new Main());
         builder.buildAsync();
+        //String[]
     }
     @Override
     public void onMessageReceived(MessageReceivedEvent event){
@@ -23,6 +27,9 @@ public class Main extends ListenerAdapter {
                 event.getMessage().getContentDisplay() );
         if(event.getMessage().getContentRaw().equals("!ping")){
             event.getChannel().sendMessage("Pong!").queue();
+        }
+        if(event.getMessage().getContentRaw().equals("!puntime")){
+            event.getChannel().sendMessage("How does a Computer get drunk? It takes screenSHOTS XD").queue();
         }
 
 
