cba4d2964257a2137208bce05e5726fdd91c5a53
Hayley Christianson <hayleychristianson@gmail.com>
2018-04-15 12:08:53 -0500

    Started on Randomizer
    Edited the Arrays for Challenges, punArray and Quotes, then put punarray through an math.rand and pulled out the value, trying to call it in the event handler, however it does not EXIST. D:

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,44 +10,44 @@ import javax.security.auth.login.LoginException;
 public class Main extends ListenerAdapter {
 
     public static void main(String[] args) throws LoginException{
+
         JDABuilder builder = new JDABuilder(AccountType.BOT);
         String token = "NDM0ODI4NzY2MzQ3NTI2MTQ1.DbQWzw.mY_YkwWN_chyztyCUPo_uWrdbek";
         builder.setToken(token);
         builder.addEventListener(new Main());
         builder.buildAsync();
-<<<<<<< HEAD
-        String[] challenges = {"",
-                                ""};
-=======
-<<<<<<< HEAD
-        //string[30] stringQuotes;
-        //punArray[20];
+        String[] challenges = {"","",
+                               "","",
+                               "","",
+                               "","",
+                               "","",
+                               "","",
+                               "","",
+                               "","",
+                               "","",};
+
+        String[] stringQuotes = {"Yesterday is not ours to recover, but tomorrow is ours to win or lose.\n" + "-Lyndon B. Johnson","Yesterday is history, tomorrow is a mystery, and today is a gift...That's why they call it present!\n" + "-Master Oogway, Kung Fu Panda ",
+                             "","",
+                             "","",
+                             "","",
+                             "","",
+                             "","",
+                             "","",
+                             "","",
+                             "","",};
+
+        String[] punArray = {"Have a cup of positiviTEA","Donut let anyone tell you you're less thanincrEDIBLE",
+                             "Life is good for Optimus Prime. It's even better for Optimist Prime.","Think like a proton. Always positive.",
+                             "Everything whale be alright.","Achieve grapeness.",
+                             "","How does a Computer get drunk? It takes screenSHOTS XD",
+                             "","",
+                             "","",
+                             "","",
+                             "","",
+                             "","",};
+        double randResult = (int)(Math.random() * punArray.length);
+        CharSequence result = punArray[(int)randResult];
     }
-
-    /*stringQuotes[0]="Yesterday is not ours to recover, but tomorrow is ours to win or lose.
-     -Lyndon B. Johnson
-     ";
-    stringQuotes[1]="Yesterday is history, tomorrow is a mystery, and today is a gift...That's why they call it present!
-    -Master Oogway, Kung Fu Panda
-    "; */
-
-    /*
-    punArray[0]="Have a cup of positiviTEA";
-    punArray[1]="Donut let anyone tell you you're less thanincrEDIBLE";
-    punArray[2]="Life is good for Optimus Prime. It's even better for Optimist Prime.";
-    punArray[3]="Think like a proton. Always positive.";
-    punArray[4]="Everything whale be alright.";
-    punArray[5]="Achieve grapeness.";
-    */
-
-=======
-        //String[]
->>>>>>> 2632005fffb8236104c0640f3d1f1f998994b281
-        //List&lt;Member&gt; mentionedMembers = commandEvent.getMessage().getMentionedMembers();
-        //Member victim = mentionedMembers.get(0);
-    }
-
->>>>>>> 0a5a5059ed953f005ebaee31c8fa840bf9c01c5c
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event){
@@ -65,7 +65,7 @@ public class Main extends ListenerAdapter {
         }
 
         if(event.getMessage().getContentRaw().equals("!puntime")){
-            event.getChannel().sendMessage("How does a Computer get drunk? It takes screenSHOTS XD").queue();
+            event.getChannel().sendMessage( "Im twigging").queue();
         }
 
         if(event.getMessage().getContentRaw().equals("damn")){
