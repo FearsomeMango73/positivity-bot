38f06ef226853e79d01e92064a1f500a5d45c5ec
Hayley Christianson <hayleychristianson@gmail.com>
2018-09-10 08:05:33 -0500

    Token Removal
    Removed Token

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,7 +14,7 @@ public class Main extends ListenerAdapter {
     public static void main(String[] args) throws LoginException{
 
         JDABuilder builder = new JDABuilder(AccountType.BOT);
-        String token = "NDM0ODI4NzY2MzQ3NTI2MTQ1.DbQWzw.mY_YkwWN_chyztyCUPo_uWrdbek";
+        String token = "NDM0ODI4NzY2MzQ3NTI2MTQ1.";
         builder.setToken(token);
         builder.addEventListener(new Main());
         builder.buildAsync();
